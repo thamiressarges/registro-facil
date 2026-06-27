e0314f1da4026be9a71e5154f546a7c2b5e5e330
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-27 12:10:30 -0400

    chore: configura ThemeProvider e tema global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {Details} from './pages/Details/index.tsx'
+
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme} >
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )

d423ef86241c2b248290e23c963919b0c42b74c7
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-27 20:13:11 -0400

    feat: adiciona seção de perfil do usuário no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles"
+import { Container, Profile } from "./styles"
 
 export function Header(){
     return(
         <Container>
+            <Profile>
+                <img src="https://github.com/thamiressarges.png" alt="Foto do usuário" />
 
+                <div>
+                    <span>Bem vindo</span>
+                    <strong>Thamires Sarges</strong>
+                </div>
+            </Profile>
 
         </Container>
     )

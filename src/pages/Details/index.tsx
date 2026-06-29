4c0049c0879dd22dd95d8213355e7b92b9de6c04
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-28 21:01:27 -0400

    feat: estrutura layout de conteúdo da página de detalhes

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,27 +1,40 @@
+import { Container, Notes, Content } from './styles'
+
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
-import { Container, Notes } from './styles'
 import { Status } from '../../components/Status'
 import { ButtonText } from '../../components/ButtonText'
+import { Button } from '../../components/Button'
 
 export function Details(){
   return(
     <Container>
       <Header/>
 
-      <ButtonText title="Excluir"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir"/>
 
-      <Section title="Links úteis">
-        <Notes>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </Notes>
-      </Section>
+          <h1>Lydia</h1>
+          <p>Descricao do pedido</p>
 
-      <Section title="Status">
-        <Status title="Pago"/>
-      </Section>
+          <Section title="Links úteis">
+            <Notes>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </Notes>
+          </Section>
+
+          <Section title="Status">
+            <Status title="Pago"/>
+          </Section>
+
+          <Button title="Voltar"/>
+        </Content>
+      </main>
+
+      
     </Container>
   )
 }

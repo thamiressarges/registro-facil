859264ac403081e951118ce816d1ad666cc77548
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-06-29 20:41:42 -0400

    feat: adiciona layout principal da página Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {Details} from './pages/Details/index.tsx'
+import {Home} from './pages/Home'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme.ts'
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme} >
       <GlobalStyle/>
-      <Details />
+      <Home />
     </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import {Container, Brand, Menu, Search, Content, NewSale} from './styles';
+
+import { Header } from '../../components/Header';
+
+export function Home(){
+    return(
+        <Container>
+            <Brand>
+                <h1>Registro Fácil</h1>
+            </Brand>
+
+            <Header/>
+
+            <Menu>
+
+            </Menu>
+
+            <Search>
+
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewSale>
+
+            </NewSale>
+
+        </Container>
+    )
+}
